85257e61a3b784a2fafa89ae82a879d3920fa26e
Steve Kahn <steve@kahn.org.uk>
2019-02-28 14:08:30 +0000

    Fixed typos in database_spec_helper

--- spec/database_setup_helper.rb
+++ spec/database_setup_helper.rb
@@ -1,13 +1,13 @@
 require 'pg'
 
 def clean_database
-  conn = PG.connect(dbname: 'database_manager_test')
+  conn = PG.connect(dbname: 'bookmark_manager_test')
   conn.exec("TRUNCATE bookmarks")
 end
 
 def build_database
-    conn = PG.connect(dbname: 'database_manager_test')
-    comn.exec("INSERT INTO bookmarks VALUES(DEFAULT, 'http://www.bbc.co.uk')")
-    comn.exec("INSERT INTO bookmarks VALUES(DEFAULT, 'http://www.twitter.com')")
-    comn.exec("INSERT INTO bookmarks VALUES(DEFAULT, 'http://www.google.com')")
+    conn = PG.connect(dbname: 'bookmark_manager_test')
+    conn.exec("INSERT INTO bookmarks VALUES(DEFAULT, 'http://www.bbc.co.uk')")
+    conn.exec("INSERT INTO bookmarks VALUES(DEFAULT, 'http://www.twitter.com')")
+    conn.exec("INSERT INTO bookmarks VALUES(DEFAULT, 'http://www.google.com')")
 end
